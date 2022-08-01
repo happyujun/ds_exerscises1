b4e17de832d36e3ec441bb9d1fb80117c8a545a1
saecomi <jjun4520@gmail.com>
2022-08-01 14:34:35 +0900

    18분 커밋2

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import streamlit as st #한번도 안쓰면 회색으로 나온다.
 st.title("데이터과학 실습 과정") #최상위 제목을 쓸 때는 title을 써줘야한다.
 st.header("1일차")
 st.subheader("파이참 설정 및 사용법")
-st.write("Normal Text")
-st.write("tttNormal Text")
 
+st.sidebar.selectbox('메뉴',['Home','입력','조회'])
 
 #test
