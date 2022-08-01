36bf7ac8a2ba7cec85f2a0bee80d8ca1ae5d2b2e
이유준 <saecomi@iyujun-ui-MacBookAir.local>
2022-08-01 13:57:03 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+#순수 파이썬이 아니기 때문에 import 로 시작해야한다.
+
+import streamlit as st #한번도 안쓰면 회색으로 나온다.
+
+st.title("데이터과학 실습 과정") #최상위 제목을 쓸 때는 title을 써줘야한다.
+st.header("1일차")
+st.subheader("파이참 설정 및 사용법")
+
+
